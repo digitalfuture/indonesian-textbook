e4ad86e344ec8b6973dd7e9e5b865710021e2489
avorio <avorio@mail.ru>
2026-05-16 10:21:22 +0700

    Components update

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,9 @@
 import { createApp } from "vue";
 import { createPinia } from "pinia";
+import PrimeVue from "primevue/config";
+import Aura from "@primeuix/themes/aura";
+import "primeicons/primeicons.css";
+
 import App from "./App.vue";
 import router from "./router";
 import "./styles/main.css";
@@ -8,5 +12,13 @@ const app = createApp(App);
 
 app.use(createPinia());
 app.use(router);
+app.use(PrimeVue, {
+  theme: {
+    preset: Aura,
+    options: {
+      darkModeSelector: ".dark-mode",
+    },
+  },
+});
 
 app.mount("#app");
